9ea9d57700f045dd52825079c61ba989266ecb9d
ElizandroLima <elizandro.slima@gmail.com>
2015-04-16 11:11:14 -0300

    #CsF - Adicionado Mapa mundi o botão todos
    Adicionado Mapa mundi o botão todos

--- index_students.php
+++ index_students.php
@@ -8,7 +8,7 @@
 
 ?>
 <!--<body style="font-family: Arial;border: 0 none;">-->
-<div style="text-align: justify" align="center">
+<div style="text-align: justify">
 <?php 
 	$txt = msg("csf_mapa");
 	$txt = troca($txt,' 50 ',' '.$total.' '); 
@@ -24,14 +24,15 @@
 ?></div>
 <br/><br/>
 <!--Chama metodo que gera os mapas e tabela dinamicos -->
-<?php echo $csf->world_mapa_estudantes(); ?>
+	<center><?php echo $csf->world_mapa_estudantes(); ?></center>
 	
+	<!--
 	<div style="text-align: justify">
 		<BR><BR><center><h2><?=msg('est_no_ext');?></h2></center>
 	   	<div style="text-align: justify">
 	   		<?php echo msg("csf_mapa_foot"); ?>
 	   	</div>
 		<!--Chama metodo que gera a relacao dos alunos por curso e pais -->		
-		<?php echo $csf->lista_inscritos();?>
-	</div>
+		<!--<?php echo $csf->lista_inscritos();?></div>-->
+	
 	
--- mapa-csf-pucpr.php
+++ mapa-csf-pucpr.php
@@ -3,56 +3,28 @@ require('cab.php');
 ?>
   <body>
     <div class="navbar-wrapper">
-      <div class="container">
-
-        <?php
-        require('componentes/nav.php');
-        ?>
-
-        <div class="page-header">
-          <h1>Mapa dos bolsistas Ciência sem Fronteiras</h1>
-        </div>
-
-        <div class="indicadores-paragrafo">
-
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
-
-        </div>
-        
-
-      	<?php
-        	require('index_students.php');
-        ?>
-
-
-      	<!-- FOOTER -->
-      	<?php
-        	require('componentes/footer.php');
-        ?>
-
-    </div><!-- /.container -->
-
+      	<div class="container">
+	        <?php
+	        require('componentes/nav.php');
+	        ?>
+	        <div class="page-header">
+	          <h1>Mapa dos bolsistas Ciência sem Fronteiras</h1>
+	        </div>
+	        <div class="indicadores-paragrafo">
+	          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+	        </div>
+	      	<?php
+	        require('index_students.php');
+	        ?>
+	      	<!-- FOOTER -->
+	      	<?php
+	        require('componentes/footer.php');
+	        ?>
+    	</div><!-- /.container -->
     </div>
   </div>
 
+   
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="js/vendor/holder.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-
-    <script src="js/cbpFWTabs.js"></script>
-    <script>
-      (function() {
-        [].slice.call( document.querySelectorAll( '.tabs' ) ).forEach( function( el ) {
-          new CBPFWTabs( el );
-        });
-      })();
-    </script>
   </body>
 </html>
